1156d0a4f17994f58690dd7e82172a067428ebbb
ChangJoo Park <pcjpcj2@gmail.com>
2019-07-23 09:57:06 +0900

    Initial Commit

---
+++ uuidv4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  uuidv4
+//
+//  Created by ChangJoo Park on 23/07/2019.
+//  Copyright © 2019 ChangJoo Park. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ uuidv4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  uuidv4
+//
+//  Created by ChangJoo Park on 23/07/2019.
+//  Copyright © 2019 ChangJoo Park. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
